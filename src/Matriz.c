d102c94eb06668c121c117dc847d2c158051332b
ohenriquesouza <henriquesouzafagundes2003@gmail.com>
2022-04-07 11:47:46 -0300

    first commit Etapa2

--- src/Matriz.c
+++ src/Matriz.c
@@ -16,17 +16,20 @@ void Mostra_Matriz(int Tamanho_matriz, int **matriz){
     for(int i = 0; i < Tamanho_matriz; i++){
         printf("\n");
         for(int j = 0; j < Tamanho_matriz; j++){
-            printf(" %d ", matriz[i][j]);
+            printf("%d\t", matriz[i][j]);
         }
     }
 }
 
 void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
+    int Guarda_numeros[50], posicao = 0;
     int soma = 0, i = 0, j = 0;
     bool invalido = false;
     soma = matriz[i][j];
+    Guarda_numeros[posicao] = soma;
 
     while(i != Tamanho_matriz  && j != Tamanho_matriz  ){
+        posicao++;
 
         //CASO ESTEJA NA PRIMEIRA COLUNA//
         if(j == 0){
@@ -34,6 +37,7 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
             //BAIXO MAIOR - PRIMEIRA COLUNA
             if(i < Tamanho_matriz - 1 && matriz[i + 1][j] >= matriz[i][j + 1]){
                 soma += matriz[i + 1][j];
+                Guarda_numeros[posicao] = matriz[i + 1][j];
                 i++;
                 matriz[i - 1][j] = invalido;
             }
@@ -41,6 +45,7 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
             //DIREITA MAIOR - PRIMEIRA COLUNA//
             else{
                 soma += matriz[i][j + 1];
+                Guarda_numeros[posicao] = matriz[i][j + 1];
                 j++;
                 matriz[i][j - 1] = invalido;
             }
@@ -49,6 +54,7 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
         //ULTIMA LINHA//
         else if(i == Tamanho_matriz - 1 && j <= Tamanho_matriz - 1){
             soma += matriz[i][j + 1];
+            Guarda_numeros[posicao] = matriz[i][j + 1];
             j++;
             matriz[i][j - 1] = invalido;
         }
@@ -57,10 +63,12 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
         else if( i != Tamanho_matriz -1 && j == Tamanho_matriz -1){
             if(matriz[i + 1][j] > matriz[i][j - 1]){
                 soma += matriz[i + 1][j];
+                Guarda_numeros[posicao] = matriz[i + 1][j];
                 i++;
                 matriz[i - 1][j] = invalido;
             }else{
                 soma += matriz[i][j - 1];
+                Guarda_numeros[posicao] = matriz[i][j - 1];
                 j--;
                 matriz[i][j + 1] = invalido;
             }
@@ -71,6 +79,7 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
             //ESQUERDA MAIOR QUE BAIXO E DIREITA//
             if(matriz[i][j - 1] > matriz[i + 1][j] && matriz[i][j - 1] > matriz[i][j + 1] ){
                 soma += matriz[i][j - 1];
+                Guarda_numeros[posicao] = matriz[i][j - 1];
                 j--;
                 matriz[i][j + 1] = invalido;
             }
@@ -78,6 +87,7 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
             //BAIXO MAIOR QUE ESQUERDA E DIREITA//
             else if(matriz[i + 1][j] > matriz[i][j - 1] && matriz[i + 1][j] > matriz[i][j + 1] ){
                 soma += matriz[i + 1][j];
+                Guarda_numeros[posicao] = matriz[i + 1][j];
                 i++;
                 matriz[i - 1][j] = invalido;
             }
@@ -85,6 +95,7 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
             //DIREITA MAIOR QUE ESQUERDA E BAIXO//
             else if(matriz[i][j + 1] > matriz[i][j - 1] && matriz[i][j + 1] > matriz[i + 1][j] ){
                 soma += matriz[i][j + 1];
+                Guarda_numeros[posicao] = matriz[i][j + 1];
                 j++;
                 matriz[i][j - 1] = invalido;
             }
@@ -92,12 +103,14 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
             //VIZINHOS IGUAIS //
             else if(matriz[i][j + 1] == matriz[i + 1][j]){
                 soma += matriz[i][j + 1];
+                Guarda_numeros[posicao] = matriz[i][j + 1];
                 j++;
                 matriz[i][j - 1] = invalido;
             }
 
             else if(matriz[i][j + 1] == matriz[i][j - 1]){
                 soma += matriz[i][j + 1];
+                Guarda_numeros[posicao] = matriz[i][j + 1];
                 j++;
                 matriz[i][j - 1] = invalido;
             }
@@ -106,6 +119,16 @@ void Anda_pela_matriz(int Tamanho_matriz, int **matriz){
     printf("\n--------------------------");
     printf("\nSoma total: %d\n", soma);
     printf("--------------------------");
+    printf("\nNumero utilizados para trilhar o caminho: ");
+    for(i = 0; i < posicao; i++){
+        printf("%d, ", Guarda_numeros[i]);
+    }
+    printf("\n--------------------------");
+    printf("\nNumeros utilizados para trilhar o caminho ao contrário: ");
+    for(i = posicao - 1; i >= 0; i --){
+        printf("%d, ", Guarda_numeros[i]);
+    }
+    printf("\n--------------------------");
     printf("\nCaminho percorrido: ");
     Mostra_Matriz(Tamanho_matriz, matriz);
 }
